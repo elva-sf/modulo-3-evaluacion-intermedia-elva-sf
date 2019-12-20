53759f3af26dbf5491b170952b9cf963a3b12923
Elva Souto Fernández <e.souto.fernandez@gmail.com>
2019-12-20 08:32:02 +0100

    initial commit

---
+++ docs/precache-manifest.122b5f23b5e1d08910d4d5b008b63c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d72e6d4f2b60b4a6c32433baa1670a",
+    "url": "/./index.html"
+  },
+  {
+    "revision": "54bd99684dd50aea861d",
+    "url": "/./static/css/main.d3aaa3ee.chunk.css"
+  },
+  {
+    "revision": "7a8b3ee02625380cc489",
+    "url": "/./static/js/2.e8948418.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/./static/js/2.e8948418.chunk.js.LICENSE"
+  },
+  {
+    "revision": "54bd99684dd50aea861d",
+    "url": "/./static/js/main.8e7e9b99.chunk.js"
+  },
+  {
+    "revision": "24812ec20c69ca099d44",
+    "url": "/./static/js/runtime-main.d7998b48.js"
+  }
+]);
